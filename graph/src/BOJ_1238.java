b27e2b7b65820940aa1f23055ec4b6cab0cf1bf3
Theodore <bethejustice@naver.com>
2018-09-24 15:58:24 +0900

    Solve 1238 by floyd algo

--- graph/src/BOJ_1238.java
+++ graph/src/BOJ_1238.java
@@ -1,142 +1,72 @@
 import java.io.BufferedReader;
-        import java.io.IOException;
-        import java.io.InputStreamReader;
-        import java.util.Arrays;
-        import java.util.PriorityQueue;
-        import java.util.Queue;
-        import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class BOJ_1238 {
+    static int MAX_TIME = 99000;
 
-    static BufferedReader br;
+    static int[][] d;
+    static int N; // the number of student
+    static int M; // the number of road
+    static int X; // goal index
+
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static int[][] map;
-    static int[] dist;
-    static boolean[] visit;
-    static int N;
-    static int M;
-    static int X;
 
     public static void main(String[] args) throws IOException {
 
+        // init
+        N = readInt();
+        M = readInt();
+        X = readInt();
+        d = new int[N + 1][N + 1];
 
-        br =  new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        X = Integer.parseInt(st.nextToken());
-
-        map = new int[N+1][N+1];
-        dist = new int[N+1];
-        visit = new boolean[N+1];
-
-        for(int i = 0 ; i < M ; i++){
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            map[x][y]=v;
+        for(int i = 1; i <= N; i++){
+            Arrays.fill(d[i], MAX_TIME);
+            d[i][i] = 0;
         }
 
-        Arrays.fill(dist, 102);
-        dist[X]=0;
+        for (int i = 1; i <= M; i++) {
+            int fromIndex = readInt();
+            int toIndex = readInt();
+            int value = readInt();
 
-        Queue<Integer> que = new PriorityQueue<>();
-        int cnt = 0;
-        int cur = X;
-
-        while(cnt < N) {
-            //거리 업데이트 하기
-            for (int i = 1; i <= N; i++){
-                if(visit[i]) continue;
-                if(map[cur][i]==0) continue;
-                if(dist[cur]+map[cur][i] < dist[i]) dist[i] = dist[cur]+map[cur][i];
-            }
-            visit[cur] = true;
-
-            //가장짧은 노드 고르기
-            int min =102;
-            for(int i = 1; i <= N; i++){
-                if(visit[i]) continue;
-                if(dist[i]==0) continue;
-                if( dist[i] < min) {
-                    min = dist[i];
-                    cur = i;
-                }
-
-            }
-            cnt++;
+            d[fromIndex][toIndex] = value;
         }
 
+        floyd();
+
+        // solve
         int max = 0;
         for(int i = 1; i <= N; i++){
-            if( dist[i] == 102) continue;
-            max = Math.max(max, dist[i]);
-        }
-
-        int[] ans = Arrays.copyOf(dist, N+1);
-
-
-        int max2 =0;
-        for(int i = 1; i <= N; i++){
-
             if(i == X) continue;
-
-            visit = new boolean[N+1];
-            dist = new int[N+1];
-
-            ans[i] = ans[i] + dist(i, X);
-            max2 = Math.max(max2, ans[i]);
+            max = Math.max(max , d[i][X] + d[X][i]);
         }
 
-        System.out.println(max2);
-
-    }// main
-
-    public static int dist(int s, int e){
-        int[] dist = new int[N+1];
-        boolean[] visit = new boolean[N+1];
-
-        Arrays.fill(dist, 102);
-        dist[s]=0;
-
-        Queue<Integer> que = new PriorityQueue<>();
-        int cnt = 0;
-        int cur = s;
-
-        while(cnt < N) {
-            //거리 업데이트 하기
-            for (int i = 1; i <= N; i++){
-                if(visit[i]) continue;
-                if(map[cur][i]==0) continue;
-                if(dist[cur]+map[cur][i] < dist[i]) dist[i] = dist[cur]+map[cur][i];
-            }
-            visit[cur] = true;
-
-            //가장짧은 노드 고르기
-            int min =102;
-            for(int i = 1; i <= N; i++){
-                if(visit[i]) continue;
-                if(dist[i]==0) continue;
-                if( dist[i] < min) {
-                    min = dist[i];
-                    cur = i;
-                }
-
-            }
-            cnt++;
-
-            if(visit[e]) break;
-        }
-
-        int max = 0;
-        for(int i = 1; i <= N; i++){
-            if( dist[i] == 102) continue;
-            max = Math.max(max, dist[i]);
-        }
-
-        return dist[e];
+        // print
+        System.out.println(max);
     }
 
+    static void floyd() {
+
+        for(int k = 1; k <= N; k++){
+            for(int i = 1; i <= N; i++){
+                for(int j = 1; j <= N; j++){
+                    d[i][j] = d[i][j] > d[i][k] + d[k][j] ? d[i][k] + d[k][j] : d[i][j];
+                }
+            }
+        }
+
+    }
+
+    static int readInt() throws IOException {
+        if (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine());
+        }
+
+        return Integer.parseInt(st.nextToken());
+    }
 }
