4b081d35ef769ae8c9e18d0380d251e38a255c99
Theodore <bethejustice@naver.com>
2018-11-23 14:42:22 +0900

    solve: 1476 날짜계산

---
+++ src/etc/BOJ_1476.java
@@ -0,0 +1,21 @@
+package etc;
+
+import java.util.Scanner;
+
+public class BOJ_1476 {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int E = sc.nextInt();
+        int S = sc.nextInt();
+        int M = sc.nextInt();
+
+        int year = 1;
+        while( (year-E)%15 != 0 || (year-S)%28 != 0 || (year-M)%19 != 0){
+            year++;
+        }
+
+        System.out.println(year);
+    }
+
+}
