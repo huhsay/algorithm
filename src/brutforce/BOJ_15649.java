43515c76f42e19bcdbce3f8e1e4ae6ca5187c6b6
Theodore <bethejustice@naver.com>
2018-11-26 23:09:37 +0900

    solve: 10972 permitation

--- src/brutforce/BOJ_15649.java
+++ src/brutforce/BOJ_15649.java
@@ -1,12 +1,40 @@
 package brutforce;
 
-public class BOJ_15649 {
-    private static BOJ_15649 ourInstance = new BOJ_15649();
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
 
-    public static BOJ_15649 getInstance() {
-        return ourInstance;
+public class BOJ_15649 {
+    static int n;
+    static int m;
+    static Deque<Integer> que;
+    static boolean[] visited = new boolean[9];
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        que = new LinkedList<>();
+
+        dfs();
     }
 
-    private BOJ_15649() {
+    public static void dfs(){
+        if(que.size() == m){
+            for ( int number : que){
+                System.out.print(number + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if(!visited[i]){
+                visited[i] = true;
+                que.add(i);
+                dfs();
+                visited[i] = false;
+                que.removeLast();
+            }
+        }
     }
 }
--- src/brutforce/BOJ_9095.java
+++ src/brutforce/BOJ_9095.java
@@ -1,4 +1,37 @@
 package brutforce;
 
+import java.util.Scanner;
+
 public class BOJ_9095 {
+    static int[] dp;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        dp = new int[12];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for (int t = 0; t < T; t++) {
+            System.out.println(solve(sc.nextInt()));
+        }
+    }
+
+    public static int solve(int i) {
+        int ans = 0;
+
+        if(i == 1) return 1;
+        if(i == 2) return 2;
+        if(i == 3) return 4;
+
+        for (int j = 1; j <= 3; j++) {
+            if (dp[i - j] != 0) {
+                ans += dp[i - j];
+                continue;
+            }
+            ans += solve(i - j);
+        }
+        dp[i] = ans;
+        return ans;
+    }
 }
