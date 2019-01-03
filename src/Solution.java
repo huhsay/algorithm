b2ad75a4f305e542b72f7fdd10742eded6c4b99e
Theodore <bethejustice@naver.com>
2019-01-03 19:34:40 +0900

    fix: update gitignore

--- src/Solution.java
+++ src/Solution.java
@@ -1,10 +1,34 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import org.junit.jupiter.api.Test;
 
-public class Solution {
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class Solution{
+    public static List<String> streamEX(String[] input){
+
+        return Arrays.stream(input).map( x -> { if (x.contains("s")){ return "x" ;}
+        else{ return x;}}).collect(Collectors.toList());
+
+        Boolean bool = new Boolean("true");
+        String
+
+    }
+
+    @Test
+    public void solution1(){
+        Date date = new Date();
+        date.getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String dateString = "1992-05-11";
+
+        try {
+            System.out.println(sdf.parse(dateString).toString());
+        }catch(Exception e){
+            e.getMessage();
+        }
+    }
 }
+
