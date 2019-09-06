835cee2b61a9a7540b7732601c219695684f435b
theodore <bethejustice@gmail.com>
2019-09-07 02:33:17 +0900

    solve 백준 10171 고양이

---
+++ src/etc/BOJ_10171.java
@@ -0,0 +1,16 @@
+package etc;
+
+/*
+ 백준 10171 고양이
+ 스트링 출력
+ 20190907
+ */
+
+public class BOJ_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
